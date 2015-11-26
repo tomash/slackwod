8c195a948bbf56a3309513717cdf0d7a122f95b6
Tomasz Stachewicz <tomekrs@o2.pl>
2015-11-26 18:57:22 +0100

    post url, cleanup of message content (no hashtags)

--- app.rb
+++ app.rb
@@ -8,6 +8,15 @@ get '/' do
   graph = Koala::Facebook::API.new(access_token)
   posts = graph.get_connection("CrossFitELEKTROMOC", "posts")
   wod_posts = posts.select { |post| post["message"].strip.match /^WOD#/ }
-  latest_wod = wod_posts.first
-  latest_wod["message"]
+  wod_post = wod_posts.first
+  wod_text = wod_post["message"]
+  wod_text.gsub!(/#(\w+)/, "")
+  wod_text.squeeze!("\n").strip!
+  page_id, item_id = wod_post["id"].split("_")
+
+  returned = "
+#{wod_text}
+
+url: https://www.facebook.com/#{page_id}/posts/#{item_id}"
+
 end
