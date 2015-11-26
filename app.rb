d54891d53d7030dc75f9e8a575b1dfb3c9fa4237
Tomasz Stachewicz <tomekrs@o2.pl>
2015-11-26 18:44:22 +0100

    actually fetch stuff from fb and return latest wod

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
-require 'json'
+require 'koala'
+require 'dotenv'
+Dotenv.load
 
 get '/' do
-  ""
+  access_token = ENV["ACCESS_TOKEN"]
+  graph = Koala::Facebook::API.new(access_token)
+  posts = graph.get_connection("CrossFitELEKTROMOC", "posts")
+  wod_posts = posts.select { |post| post["message"].strip.match /^WOD#/ }
+  latest_wod = wod_posts.first
+  latest_wod["message"]
 end
