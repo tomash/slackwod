9fa0371dc986ba772400dd0f5cb4e66680923c37
Tomasz Stachewicz <tomekrs@o2.pl>
2015-11-27 15:43:51 +0100

    prevent 'added an event' post from breaking our wod_posts finder. prepare sample posts.json for specs.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ get '/' do
   access_token = ENV["ACCESS_TOKEN"]
   graph = Koala::Facebook::API.new(access_token)
   posts = graph.get_connection("CrossFitELEKTROMOC", "posts")
-  wod_posts = posts.select { |post| post["message"].strip.match /^WOD#/ }
+  wod_posts = posts.select { |post| post["message"].to_s.strip.match /^WOD#/ }
   wod_post = wod_posts.first
   wod_text = wod_post["message"]
   wod_text.gsub!(/#(\w+)/, "")
